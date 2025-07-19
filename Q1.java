75e3c585c73931001aa784c8da5413d0af6f6928
Farhan Pathan <farhanpathan858@gmail.com>
2025-07-19 23:35:36 +0530

    Questions and its Solution - Gryphone Training

---
+++ Q1.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();       // Number of elements
+        int[] arr = new int[n];     // Array to store elements
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();  // Input elements
+        }
+
+        int cnt = 1;                // Count starts at 1 (first element is always counted)
+        int max = arr[0];           // Track maximum seen so far
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                cnt++;
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Count: " + cnt);
+    }
+}
