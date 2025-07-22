f1c3660a83dfee6203f691c1e3f164d9eb5fad54
Farhan Pathan <148573533+Farhan-Pathan-9322@users.noreply.github.com>
2025-07-22 22:21:48 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -6,8 +6,8 @@ public class Q1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();       // Number of elements
-        int[] arr = new int[n];     // Array to store elements
+        int n = sc.nextInt();       
+        int[] arr = new int[n];     // New Array created with the integer data type
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();  // Input elements
@@ -15,7 +15,6 @@ public class Q1{
 
         int cnt = 1;                // Count starts at 1 (first element is always counted)
         int max = arr[0];           // Track maximum seen so far
-
         for (int i = 1; i < n; i++) {
             if (arr[i] > max) {
                 cnt++;
